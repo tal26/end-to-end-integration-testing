a37e6e30c5df2581c900c6a9d1387534b3c8fcbd
tal <xacfez-zaqsu5-Wimrah>
2022-03-01 12:30:03 +0100

    added objects to Links at the bottom

---
+++ src/test/java/sickag/assethub/pageobject/links/ImprintPage.java
@@ -0,0 +1,15 @@
+package sickag.assethub.pageobject.links;
+
+import sickag.assethub.pageobject._AbstractPageAssetHub;
+
+public class ImprintPage extends _AbstractPageAssetHub {
+    @Override
+    protected void makeSure() {
+
+    }
+
+    @Override
+    public void open() {
+
+    }
+}
---
+++ src/test/java/sickag/assethub/pageobject/links/TermsOfUsePage.java
@@ -0,0 +1,15 @@
+package sickag.assethub.pageobject.links;
+
+import sickag.assethub.pageobject._AbstractPageAssetHub;
+
+public class TermsOfUsePage extends _AbstractPageAssetHub {
+    @Override
+    protected void makeSure() {
+
+    }
+
+    @Override
+    public void open() {
+
+    }
+}
